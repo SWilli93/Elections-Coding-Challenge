eb1787f4822f27fe327d81815ecd0183f58374c4
Scott Williams <ScottWilliamsSolutions@gmail.com>
2021-12-02 13:01:20 -0500

    steps 1 -3

--- Election Results Coding Challenge.py
+++ Election Results Coding Challenge.py
@@ -5,3 +5,15 @@ survey_responses = ['Ceballos', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos','
 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos',
 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos',
 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos']
+
+# Total voted for Ceballos
+total_ceballos = sum([1 for response in survey_responses if response == 'Ceballos'])
+print(total_ceballos)
+
+# length of the survey
+survey_length = float(len(survey_responses))
+# print(survey_length)
+
+#percentage of voters who voted Ceballos
+percentage_ceballos = total_ceballos / survey_length
+# print(percentage_ceballos)
